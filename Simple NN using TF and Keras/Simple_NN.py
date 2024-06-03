ffc5ff174b8c32b3e5633c6d8c5a9d38f54d1b56
Aaditya <aadityakumar26082004@gmail.com>
2024-06-03 22:57:09 +0530

    Update Simple_NN.py
    Number of Epochs, Learning Rate, Targets

--- Simple NN using TF and Keras/Simple_NN.py
+++ Simple NN using TF and Keras/Simple_NN.py
@@ -1,101 +1,61 @@
-# We must always import the relevant libraries for our problem at hand. NumPy and TensorFlow are required for this example.
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# First, we should declare a variable containing the size of the training set we want to generate.
+# Declare a variable containing the size of the training set we want to generate.
 observations = 100000
 
-# We will work with two variables as inputs. You can think about them as x1 and x2 in our previous examples.
-# We have picked x and z, since it is easier to differentiate them.
-# We generate them randomly, drawing from an uniform distribution. There are 3 arguments of this method (low, high, size).
-# The size of xs and zs is observations x 1. In this case: 1000 x 1.
+# Generate random inputs.
 xs = np.random.uniform(low=-10, high=10, size=(observations,1))
 zs = np.random.uniform(-10, 10, (observations,1))
 
-# Combine the two dimensions of the input into one input matrix. 
-# This is the X matrix from the linear model y = x*w + b.
-# column_stack is a Numpy method, which combines two matrices (vectors) into one.
+# Combine the two dimensions of the input into one input matrix.
 generated_inputs = np.column_stack((xs,zs))
 
-# We add a random small noise to the function i.e. f(x,z) = 2x - 3z + 5 + <small noise>
+# Add a random small noise to the function.
 noise = np.random.uniform(-1, 1, (observations,1))
 
-# Produce the targets according to our f(x,z) = 2x - 3z + 5 + noise definition.
-# In this way, we are basically saying: the weights should be 2 and -3, while the bias is 5.
+# Produce the targets according to our function definition.
 generated_targets = 10*xs - 5*zs + 15 + noise
 
-# save into an npz file called "TF_intro"
+# Save into an npz file called "TF_intro".
 np.savez('TF_intro', inputs=generated_inputs, targets=generated_targets)
 
-# Load the training data from the NPZ
+# Load the training data from the NPZ.
 training_data = np.load('TF_intro.npz')
 
-# Declare a variable where we will store the input size of our model
-# It should be equal to the number of variables you have
+# Declare the input and output sizes of the model.
 input_size = 2
-# Declare the output size of the model
-# It should be equal to the number of outputs you've got (for regressions that's usually 1)
 output_size = 1
 
-# Outline the model
-# We lay out the model in 'Sequential'
-# Note that there are no calculations involved - we are just describing our network
+# Outline the model.
 model = tf.keras.Sequential([
-                            # Each 'layer' is listed here
-                            # The method 'Dense' indicates, our mathematical operation to be (xw + b)
-                            tf.keras.layers.Dense(output_size,
-                                                 # there are extra arguments you can include to customize your model
-                                                 # in our case we are just trying to create a solution that is 
-                                                 # as close as possible to our NumPy model
-                                                 kernel_initializer=tf.random_uniform_initializer(minval=-0.1, maxval=0.1),
-                                                 bias_initializer=tf.random_uniform_initializer(minval=-0.1, maxval=0.1)
-                                                 )
-                            ])
+    tf.keras.layers.Dense(output_size, 
+                         kernel_initializer=tf.random_uniform_initializer(minval=-0.1, maxval=0.1),
+                         bias_initializer=tf.random_uniform_initializer(minval=-0.1, maxval=0.1)
+    )
+])
 
-# We can also define a custom optimizer, where we can specify the learning rate
+# Define a custom optimizer.
 custom_optimizer = tf.keras.optimizers.SGD(learning_rate=0.1)
-# Note that sometimes you may also need a custom loss function 
-# That's much harder to implement and won't be covered in this course though
 
-# 'compile' is the place where you select and indicate the optimizers and the loss
+# Compile the model.
 model.compile(optimizer=custom_optimizer, loss='huber_loss')
 
-# finally we fit the model, indicating the inputs and targets
-# if they are not otherwise specified the number of epochs will be 1 (a single epoch of training), 
-# so the number of epochs is 'kind of' mandatory, too
-# we can play around with verbose; we prefer verbose=2
+# Fit the model.
 model.fit(training_data['inputs'], training_data['targets'], epochs=10, verbose=2)
 
-
-# Extracting the weights and biases is achieved quite easily
-model.layers[0].get_weights()
-
-
-# We can save the weights and biases in separate variables for easier examination
-# Note that there can be hundreds or thousands of them!
+# Extract the weights and biases.
 weights = model.layers[0].get_weights()[0]
-weights
-
-
-# We can save the weights and biases in separate variables for easier examination
-# Note that there can be hundreds or thousands of them!
 bias = model.layers[0].get_weights()[1]
-bias
 
-# We can predict new values in order to actually make use of the model
-# Sometimes it is useful to round the values to be able to read the output
-# Usually we use this method on NEW DATA, rather than our original training data
+# Predict new values.
 model.predict_on_batch(training_data['inputs']).round(1)
 
-# If we display our targets (actual observed values), we can manually compare the outputs and the targets
+# Display the targets.
 training_data['targets'].round(1)
 
-
-# The model is optimized, so the outputs are calculated based on the last form of the model
-
-# We have to np.squeeze the arrays in order to fit them to what the plot function expects.
-# Doesn't change anything as we cut dimensions of size 1 - just a technicality.
+# Plot the outputs and targets.
 plt.plot(np.squeeze(model.predict_on_batch(training_data['inputs'])), np.squeeze(training_data['targets']))
 plt.xlabel('outputs')
 plt.ylabel('targets')
